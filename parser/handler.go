2b6cee965253ef152c4c184484567083178535d5
soumik1987 <soumikghosh1987@gmail.com>
2024-05-07 14:16:12 +0700

    Sample API request

--- parser/handler.go
+++ parser/handler.go
@@ -22,6 +22,7 @@ func(p *ParserHandler) HealthCheck(c echo.Context) error {
 	return c.String(http.StatusOK, "Alive!")
 }
 
+// localhost:8080/get_current_block
 func(p *ParserHandler) GetCurrentBlock(c echo.Context) error {
 	// return c.JSON(http.StatusInternalServerError, err.Error())
 	res := p.ethParser.GetCurrentBlock()
@@ -29,6 +30,7 @@ func(p *ParserHandler) GetCurrentBlock(c echo.Context) error {
   	return c.JSON(http.StatusOK, res)
 }
 
+// localhost:8080/subscribe?address=0xaA247c0D81B83812e1ABf8bAB078E4540D87e3fB
 func(p *ParserHandler) Subscribe(c echo.Context) error {
 	address := c.QueryParam("address")
 
@@ -36,6 +38,7 @@ func(p *ParserHandler) Subscribe(c echo.Context) error {
 	return c.JSON(http.StatusOK, res)
 }
 
+// localhost:8080/get_transactions?address=0xaA247c0D81B83812e1ABf8bAB078E4540D87e3fB
 func(p *ParserHandler) GetTransactions(c echo.Context) error {
 	address := c.QueryParam("address")
 	res := p.ethParser.GetTransactions(address)
